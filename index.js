ae158ab4dc3c236b9edf18e746539751fbd266fe
DoctorNSA <40806022+DoctorNSA@users.noreply.github.com>
2020-02-23 23:58:47 +0100

    automatically detect maxVolume

--- index.js
+++ index.js
@@ -30,16 +30,17 @@ function MusicCastTV(log, config) {
 			that.log(error.message);
 		}
 		att=JSON.parse(body);
-		that.log('HTTP GetStatus result: Volume = ' + att.volume);
+		that.log('HTTP GetStatus result: volume = ' + att.volume);
 		that.volume = config["volume"] || att.volume;
+		that.log('HTTP GetStatus result: maximal volume = ' + att.max_volume);
+		that.maxVol = config["maxVol"] || att.max_volume;
+		that.log("volume: " + that.volume + " maxVol: " + that.maxVol);
 	});
-	this.maxVol = config["maxVol"] || 160;
 	this.inputs =  config["inputs"] || {"AirPlay": "1. 'inputs' missing", "bluetooth": "2. in config.json", "spotify": "3. please modify"};
 	this.active = config["active"] || config["power"] || 0;
 	this.ActiveIdentifier = config["identifier"] || 1;
 	this.mute = 1;
 	//this.brightness = config["brightness"] || 100;
-	this.volume = config["volume"] || vol;
 	//this.updateInterval = 1000;
 	this.version = require("./package.json").version;
 	this.features = {};
@@ -235,7 +236,6 @@ function MusicCastTV(log, config) {
 		}
 	}
 	this.log("Initialized '" + this.name + "'");
-
 }
 
 MusicCastTV.prototype = {
